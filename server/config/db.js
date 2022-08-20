65da74a53c012a2d08c2ccd7790d9ed9715ece05
shoaib <shoaib.doulatabad@gmail.com>
2022-08-20 12:41:17 +0400

    db connection with query implementation

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URL);
+
+  console.log(
+    `MongoDB connetected : ${conn.connection.host}`.cyan.underline.bold
+  );
+};
+
+module.exports = connectDB;
